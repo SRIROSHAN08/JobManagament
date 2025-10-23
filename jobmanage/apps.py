9e50ccf2004ef85ae67b32af0783490e5dc6e8b6
Sriroshan M <sriroshanro8@gmail.com>
2025-10-23 12:30:19 +0530

    Superuser added

--- jobmanage/apps.py
+++ jobmanage/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import User
+from django.db.utils import OperationalError
 
 class JobmanageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jobmanage'
+    name = 'jobmanage'  # use your actual app name here
+
+    def ready(self):
+        try:
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser('roshan', 'sriroshanro8@gmail.com', 'Roshan@08')
+        except OperationalError:
+            pass
